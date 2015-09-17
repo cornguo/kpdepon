5ef9c02ffd8c7536ecd2f9dec5c6e967a4dd78f5
CornGuo <cornguo@msn.com>
2015-09-18 01:36:27 +0800

    style

--- func.js
+++ func.js
@@ -1,4 +1,4 @@
-$(document).keydown(function(e) {
+$(document).keydown(function (e) {
     if(90 === e.which || 88 === e.which) {
         switch (e.which) {
         case 90:
@@ -42,11 +42,11 @@ var stat = [];
 stat["#pon"] = 0;
 stat["#ka"] = 0;
 
-$(document).ready(function() {
+$(document).ready(function () {
     var clickType = ((null !== document.ontouchstart)? "click":"touchstart");
     $("#pon").bind(clickType, function () {play("#pon");});
     $("#ka").bind(clickType, function () {play("#ka");});
-    setTimeout(function() {
+    setTimeout(function () {
         $('#fb_like iframe:first').attr('src', $('#fb_like iframe:first').data('src'));
     }, 1000);
 });
